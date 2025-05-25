fef448c8b0af9e7fe31f2eb13b94f844465c3749
Jerome Maunga <jerhomemaunga@gmail.com>
2025-05-25 23:43:48 +0300

    Changes to the value argument file

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,12 +1,13 @@
 const args = process.argv.slice(2);
 
-if (args.length === 0) {
-    console.log("No argument");
-
-} else if (args.length === 1) {
-    console.log("Argument foung:", args[0])
+if (!args[0]) {
+    console.log("No argument"); 
+} else if (!args[1]) {
+    console.log("Argument found:", args[0]);
 }
 else {
     console.log("Arguments found:", args.join(", "));
 }
+
+
 
