b4d00c430511121205cb21312b981ca0b4d8240d
Jerome Maunga <jerhomemaunga@gmail.com>
2025-05-25 23:15:00 +0300

    The 3 Languages js file

---
+++ 1-multi_languages.js
@@ -0,0 +1,9 @@
+let firstLine = "C is fun";
+let secondLine = "Python is cool";
+let thirdLine = "JavaScript is amazing";
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
+
+
